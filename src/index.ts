1f96b919c8584e0ec0cf2fe0c7bf9ab95ea5751d
Vincent Girard <vinccool96@gmail.com>
2022-10-23 15:29:25 -0400

    Doc and settings

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import v8 from "v8"
 import vm from "vm"
 
 v8.setFlagsFromString('--expose_gc');
+
+/**
+ * Starts the garbage collector
+ */
 export const gc: () => void = vm.runInNewContext('gc');
 
 export default gc
